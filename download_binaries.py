e4cea980a5980b35c412a9cfbed54058ca58009b
Kyle Ellefsen <kellefse@uci.edu>
2017-02-24 18:44:02 -0800

    bug fix

--- download_binaries.py
+++ download_binaries.py
@@ -1,20 +1,23 @@
 from urllib.request import urlretrieve
-import zipfile
 import os, sys
+import pip
 
-numpy_url = "https://drive.google.com/open?id=0B1nx_G74rPH5R0hfSzNRU3Q0NG8"
-pyqt4_url = "https://drive.google.com/open?id=0B1nx_G74rPH5dTdVMk5zQVIzMms"
-scipy_url = "https://drive.google.com/open?id=0B1nx_G74rPH5T09pWGZZakZEams"
-urls = [numpy_url, pyqt4_url, scipy_url]
-for url in urls:
-	
-if os.path.isdir('flika'):
-	print('Flika directory already exists. No need to Download Flika.')
-	sys.exit()
-print("Downloading Flika...")
-local_filename, headers = urlretrieve('http://github.com/flika-org/flika/archive/master.zip')
-print("Successfully downloaded Flika. Extracting...")
-with zipfile.ZipFile(local_filename, "r") as zf:
-    zf.extractall()
-print("Successfully Extracted Flika.")
-os.rename('flika-master','flika')
+
+numpy_fn  = "numpy-1.12.0+mkl-cp35-cp35m-win_amd64.whl"
+scipy_fn  = "scipy-0.19.0rc2-cp35-cp35m-win_amd64.whl"
+pyqt4_fn = "PyQt4-4.11.4-cp35-cp35m-win_amd64.whl"
+names = ['numpy', 'scipy', 'PyQt4']
+filenames = [numpy_fn, scipy_fn, pyqt4_fn]
+filenames_long = ['binary_dependencies\\'+fn for fn in filenames]
+urls = ["https://dl.dropboxusercontent.com/u/78754747/binary_dependencies/"+fn for fn in filenames]
+
+for i in range(len(names)):
+    try:
+        exec("import {}".format(names[i]))
+    except ImportError:
+        if not os.path.isfile(filenames_long[i]):
+            print("Downloading {}".format(names[i]))
+            local_filename, headers = urlretrieve(urls[i])
+            os.rename(local_filename,filenames_long[i])
+            print("{} downloaded successfully".format(names[i]))
+        pip.main(['install', filenames_long[i]])
