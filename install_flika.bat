7926cdb15596205cc4e8fee851ff36e89e498ca5
Kyle Ellefsen <kellefse@uci.edu>
2017-03-10 14:37:06 -0800

    added tifffile

--- install_flika.bat
+++ install_flika.bat
@@ -7,7 +7,7 @@ SET /p VERSION=<output
 ECHO %VERSION%
 IF "%VERSION%" == "3.5" (
 	python download_binaries.py
-	pip install qtpy pyqtgraph xmltodict ipython zmq ipykernel qtconsole pyopengl nd2reader tifffile openpyxl matplotlib
+	pip install qtpy pyqtgraph xmltodict ipython zmq ipykernel qtconsole pyopengl nd2reader openpyxl
 	python download_flika.py
 	python flika/flika.py
 ) ELSE (
